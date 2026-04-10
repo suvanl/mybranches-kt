9dcbbac4f20712d7ea21d411bdeec7309cb16da6
suvanl <32813609+suvanl@users.noreply.github.com>
2026-04-10 23:55:16 +0100

    Prefer function-level `@OptIn`

--- src/nativeMain/kotlin/com/suvanl/mybranches/system/Username.kt
+++ src/nativeMain/kotlin/com/suvanl/mybranches/system/Username.kt
@@ -1,11 +1,10 @@
-@file:OptIn(ExperimentalForeignApi::class)
-
 package com.suvanl.mybranches.system
 
 import kotlinx.cinterop.ExperimentalForeignApi
 import kotlinx.cinterop.toKString
 import platform.posix.getenv
 
+@OptIn(ExperimentalForeignApi::class)
 fun currentUsername(): String {
     val username = getenv("USER")?.toKString()
     check(!username.isNullOrBlank()) { $$"Could not determine username: $USER is not set" }
