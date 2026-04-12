4e21a685668daee4d48145eb8557da1fe07900a4
suvanl <32813609+suvanl@users.noreply.github.com>
2026-04-12 16:08:29 +0100

    Extract Ready state header to separate component

---
+++ src/nativeMain/kotlin/com/suvanl/mybranches/ui/HeaderRow.kt
@@ -0,0 +1,35 @@
+package com.suvanl.mybranches.ui
+
+import androidx.compose.runtime.Composable
+import com.jakewharton.mosaic.layout.background
+import com.jakewharton.mosaic.layout.padding
+import com.jakewharton.mosaic.modifier.Modifier
+import com.jakewharton.mosaic.ui.Arrangement
+import com.jakewharton.mosaic.ui.Row
+import com.jakewharton.mosaic.ui.Text
+import com.suvanl.mybranches.ui.theme.ThemeColor
+
+@Composable
+fun HeaderRow(
+    branchPattern: String,
+    help: String,
+    showHelp: Boolean,
+    modifier: Modifier = Modifier,
+) {
+    Row(
+        horizontalArrangement = Arrangement.spacedBy(2),
+        modifier = modifier,
+    ) {
+        Text(
+            value = branchPattern,
+            color = ThemeColor.onPrimaryContainerDark,
+            modifier = Modifier
+                .background(color = ThemeColor.primaryContainerDark)
+                .padding(horizontal = 1),
+        )
+
+        if (showHelp) {
+            Text(value = help)
+        }
+    }
+}
---
+++ src/nativeMain/kotlin/com/suvanl/mybranches/ui/theme/ThemeColor.kt
@@ -0,0 +1,11 @@
+package com.suvanl.mybranches.ui.theme
+
+import com.jakewharton.mosaic.ui.Color
+
+@Suppress("unused")
+object ThemeColor {
+
+    val primaryDark = Color(120, 229, 213)
+    val primaryContainerDark = Color(0, 178, 178)
+    val onPrimaryContainerDark = Color(0, 0, 0)
+}
